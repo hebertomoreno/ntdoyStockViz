071975a14771e3f898e609f0b1717ed9703b5978
nikolasmurdock <nikolasmurdock@gmail.com>
2016-07-18 16:12:46 -0500

    ExtractingData Safely
    All Data from the csv file (date, open, high, low, close, volume and
    Adj
    Close) are read and taken into d3 safely.

---
+++ ntdoycsv.js
@@ -0,0 +1,30 @@
+
+var ntdoycsv = function()
+{
+  var parser = d3.timeParse("%Y-%m-%d");
+  var tFormat = d3.timeFormat("%d/%m/%y");
+  d3.csv("table.csv", function(dataset)
+  {
+    data = dataset.map(function(d)
+    {
+      var parDate = parser(d.Date);
+      //console.log("Date: ", parDate);
+      d.Open = +d.Open;
+      d.High = +d.High;
+      d.Low = +d.Low;
+      d.Close = +d.Close;
+      d.Volume = +d.Volume;
+      d["Adj Close"] = +d["Adj Close"];
+      //console.log("temp: ", d.temp);
+      return{"Time": parDate,
+             "Open": d.Open,
+             "High": d.High,
+             "Low": d.Low,
+             "Close": d.Close,
+             "Volume": d.Volume,
+             "AdjClose": d["Adj Close"]};
+    })
+    console.log("Stock Data: ", data);
+  })
+
+}
