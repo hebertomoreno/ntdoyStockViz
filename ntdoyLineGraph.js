373bdde9e30b4d6bfae457c0d4d54c9f92fe99b2
nikolasmurdock <nikolasmurdock@gmail.com>
2016-07-19 13:24:52 -0500

    Looking Great!!!
    The axis are where they need to be. The line is drawn. The dataset is
    enormous (every opening stock value since 1990).
    I´m merging this one.

--- ntdoyLineGraph.js
+++ ntdoyLineGraph.js
@@ -12,7 +12,9 @@ var makeLineGraph = function(data)
   var svg = d3.select("body")
               .append("svg")
               .attr("width",w + margin.left + margin.right)
-              .attr("height",h + margin.top + margin.bottom);
+              .attr("height",h + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   /***Useful Domain Variables***/
   //var minDate = getMinDate(data);
 
@@ -23,8 +25,8 @@ var makeLineGraph = function(data)
             {
               return d.Open;
             })
-  console.log("X Domain", xDom);
-  console.log("Y Domain", yDom);
+  //console.log("X Domain", xDom);
+  //console.log("Y Domain", yDom);
   /***Scales***/
   var xScale = d3.scaleTime()
                   .domain(xDom)
@@ -36,8 +38,9 @@ var makeLineGraph = function(data)
   var xAxis = d3.axisBottom()
                      /*Each axis also needs to be told on what scale to
                        operate*/
-                     .scale(xScale);
+                     .scale(xScale)
                      /*Indicates the number of ticks*/
+                     .ticks(16);
   var yAxis = d3.axisLeft()
                     /*Each axis also needs to be told on what scale to
                       operate*/
@@ -47,24 +50,29 @@ var makeLineGraph = function(data)
   /***Line Declaration***/
   var line = d3.line()
                 .x(function(d) {
-                  console.log("X being drawn in ", d.Time);
-                  console.log("Value scaled in x: ",xScale(d.Time));
+                  //console.log("X being drawn in ", d.Time);
+                  //console.log("Value scaled in x: ",xScale(d.Time));
                   return xScale(d.Time);
                 })
                 .y(function(d) {
-                  console.log("Y being drawn in ", d.Open);
-                  console.log("Value scaled in y: ",yScale(d.Open));
+                  //console.log("Y being drawn in ", d.Open);
+                  //console.log("Value scaled in y: ",yScale(d.Open));
                   return yScale(d.Open);
                 });
   /***Draw Axes***/
   svg.append("g")
      .attr("class","xaxis")
-     .attr("transform", "translate(0," + (h - padding) + ")")
+     .attr("transform", "translate(0," + h + ")")
        .call(xAxis);
  svg.append("g")
        .attr("class", "yaxis")
-       .attr("transform", "translate(" + padding + ",0)")
-       .call(yAxis);
+       .call(yAxis)
+       .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Open");
  svg.selectAll(".xaxis text")  // select all the text elements for the xaxis
          .attr("transform", function(d) {
              return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
