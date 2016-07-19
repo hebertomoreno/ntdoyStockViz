ffa574ec137fdcc811109a364b6db8980b37895a
nikolasmurdock <nikolasmurdock@gmail.com>
2016-07-19 13:15:46 -0500

    DRAWING THE LINE OH YEAH
    It draws the line now. I just need to make it a bit more INSIDE the
    graph and we should be all set. I was referencing a variable wrong.
    Anyway, thrilled!!

--- ntdoyLineGraph.js
+++ ntdoyLineGraph.js
@@ -1,6 +1,9 @@
 //Width, Height and padding
-var w = 500;
-var h = 500;
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    w = 960 - margin.left - margin.right,
+    h = 500 - margin.top - margin.bottom;
+/*var w = 500;
+var h = 500;*/
 var padding = 50;
 
 var makeLineGraph = function(data)
@@ -8,22 +11,27 @@ var makeLineGraph = function(data)
   /***Draw SVG***/
   var svg = d3.select("body")
               .append("svg")
-              .attr("width",w)
-              .attr("height",h);
+              .attr("width",w + margin.left + margin.right)
+              .attr("height",h + margin.top + margin.bottom);
   /***Useful Domain Variables***/
-  var maxDate = new Date(2016,07,15);
-  var minDate = new Date(2016,07,01);
+  //var minDate = getMinDate(data);
+
+  var xDom = d3.extent(data, function(d){
+              return d.Time;
+            })
   var yDom = d3.extent(data,function(d)
             {
               return d.Open;
             })
+  console.log("X Domain", xDom);
+  console.log("Y Domain", yDom);
   /***Scales***/
   var xScale = d3.scaleTime()
-                  .domain([minDate, maxDate])
-                  .range([padding, w-padding]);
+                  .domain(xDom)
+                  .range([0, w]);
   var yScale = d3.scaleLinear()
                   .domain(yDom)
-                  .range([h-padding,padding]);
+                  .range([h,0]);
   /****Axes Declaration****/
   var xAxis = d3.axisBottom()
                      /*Each axis also needs to be told on what scale to
@@ -36,6 +44,18 @@ var makeLineGraph = function(data)
                     .scale(yScale)
                     /*Indicates the number of ticks*/
                     .ticks(5);
+  /***Line Declaration***/
+  var line = d3.line()
+                .x(function(d) {
+                  console.log("X being drawn in ", d.Time);
+                  console.log("Value scaled in x: ",xScale(d.Time));
+                  return xScale(d.Time);
+                })
+                .y(function(d) {
+                  console.log("Y being drawn in ", d.Open);
+                  console.log("Value scaled in y: ",yScale(d.Open));
+                  return yScale(d.Open);
+                });
   /***Draw Axes***/
   svg.append("g")
      .attr("class","xaxis")
@@ -49,4 +69,15 @@ var makeLineGraph = function(data)
          .attr("transform", function(d) {
              return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
        });
+  /***Draw Line***/
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+}
+
+function getMinDate(data){
+  /***Function to get the min Date so that we can return a
+  dynamic domain to the scale.
+  (In Progress)***/
 }
